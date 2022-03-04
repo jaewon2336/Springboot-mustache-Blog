7d9b24815f32b28e6c77130ae38f657bb0207aaf
jaewon2336 <xldzjqpf1588@naver.com>
2022-03-04 23:36:41 +0900

    3. 회원가입 로그인 처리 완료

---
+++ src/main/java/site/metacoding/dbproject/domain/post/PostRepository.java
@@ -0,0 +1,7 @@
+package site.metacoding.dbproject.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
