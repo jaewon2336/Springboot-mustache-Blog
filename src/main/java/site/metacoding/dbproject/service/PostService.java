531dfc20c61b554167b050711410c37bd24290f3
jaewon2336 <xldzjqpf1588@naver.com>
2022-03-18 12:56:30 +0900

    23. 섬머노트 달기

--- src/main/java/site/metacoding/dbproject/service/PostService.java
+++ src/main/java/site/metacoding/dbproject/service/PostService.java
@@ -2,7 +2,7 @@ package site.metacoding.dbproject.service;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Page; 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
